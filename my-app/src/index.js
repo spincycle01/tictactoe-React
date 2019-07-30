6237a312eb3d9092bf02b407d4f2ab21d90ee577
Adam <spincycle01@yahoo.com>
2019-07-29 21:34:09 -0400

    passing 2 props from Board to Square, value & onClick. Removed constructor method from Square, state now stored in board, changed to props.onClick in Square

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,22 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          this.setState({ value: 'X' });
+          this.props.onClick();
         }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -33,7 +26,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
